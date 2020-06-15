ad072e811e4539a9c9be0e42a10d1feeff35e652
Adam Bennett <adam.bennett@csolsinc.com>
2020-06-15 14:27:54 -0400

    handle no location provided

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const weather = require('./weather')
 
-// Get location from user
 let location = process.argv[2]
+
 if (process.argv.length > 3) {
     for (let i = 3; i < process.argv.length; i++) {
         location += process.argv[i]
     }
+    weather.getWeather(location, (error, data) => console.log(data))
+} else if (process.argv.length < 3) {
+    console.log('Please provide a location as an additional command line argument.')
 }
 
-// Fetch weather data and print to console
-weather.getWeather(location, (error, data) => console.log(data))
+
+
+
 
