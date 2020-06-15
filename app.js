85ce2e99f8505e6b1bc68e4fc75e2298c3a5fcc7
Adam Bennett <adam.bennett@csolsinc.com>
2020-06-15 12:03:11 -0400

    7/14

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const request = require('request')
 
-const location = 'newark'
-const geourl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + location + '.json?access_token=pk.eyJ1IjoibnlveGlkZSIsImEiOiJja2Jnbmg1Y3kxNzVzMzZtZWRrYXF5MDlhIn0.hIFo0aIPoV6QvzUJkESosg&limit=1'
+const location = process.argv[2]
+const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + location + '.json?access_token=pk.eyJ1IjoibnlveGlkZSIsImEiOiJja2Jnbmg1Y3kxNzVzMzZtZWRrYXF5MDlhIn0.hIFo0aIPoV6QvzUJkESosg&limit=1'
 
-request({url: geourl, json: true}, (geoError, geoResponse) => {
-    const latitude = geoResponse.body.features[0].center[1]
-    const longitude = geoResponse.body.features[0].center[0]
-    const endpoint = 'http://api.weatherstack.com/current?access_key=717b011987400019efffa72d90251941&query=' + latitude + ',' + longitude + '&units=f'
-    request({url: endpoint, json: true}, (weatherError, weatherResponse) => {
-        const place = geoResponse.body.features[0].place_name
-        const data = 'Weather for ' + place + '\n\nForecast: ' + weatherResponse.body.current.weather_descriptions[0] + '. \nTemperature: ' + weatherResponse.body.current.temperature + '°F.\nFeels Like: ' + weatherResponse.body.current.feelslike + '°F.'
-        console.log(data)
-    })
+request({url: url, json: true}, (geoError, geoResponse) => {
+    if (geoError) {
+        console.log('Unable to convert location into Latitude/Longitude!')
+    } else if (geoResponse.body.features.length < 1) {
+        console.log('Could not find location: ' + location)
+    } else {
+        const latitude = geoResponse.body.features[0].center[1]
+        const longitude = geoResponse.body.features[0].center[0]
+        const endpoint = 'http://api.weatherstack.com/current?access_key=717b011987400019efffa72d90251941&query=' + latitude + ',' + longitude + '&units=f'
+        request({url: endpoint, json: true}, (weatherError, weatherResponse) => {
+            if (weatherError) {
+                console.log('Unable to connect to Weather Service!')
+            } else if (weatherResponse.body.error) {
+                console.log('Could not find location from Weather Service.')
+            } else {
+                const place = geoResponse.body.features[0].place_name
+                const data = 'Weather for ' + place + '\n\nForecast: ' + weatherResponse.body.current.weather_descriptions[0] + '. \nTemperature: ' + weatherResponse.body.current.temperature + '°F.\nFeels Like: ' + weatherResponse.body.current.feelslike + '°F.'
+                console.log(data)
+            }
+        })
+    }
 })
 
